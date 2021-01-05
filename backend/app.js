c9e21a12ad06a3a6347a888a518cddb15f97b2ba
04alexklink <alex_klink123@hotmail.co.uk>
2021-01-05 15:43:25 +0000

    Can create new messages. These messages get stored in the message app

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ class MessageApp {
   constructor() {
     this.messages = [];
   }
+
+  create(messageContent) {
+    this.messages.push(messageContent);
+  }
 }
 
 export default MessageApp
--- backend/app.test.js
+++ backend/app.test.js
@@ -2,9 +2,18 @@ import { expect } from "chai";
 import MessageApp from './app.js'
 
 describe("app", function() {
-  var app = new MessageApp;
+  var app;
+  beforeEach(() => {
+    app = new MessageApp;
+  })
   it("can store messages", function() {
     expect(app.messages).to.be.an('array');
     expect(app.messages.length).to.equal(0);
+  });
+  it("can create messages", function() {
+    app.create("Hello, I am a message");
+    app.create("Hello, I'm message no2")
+    expect(app.messages[0]).to.equal("Hello, I am a message");
+    expect(app.messages[1]).to.equal("Hello, I'm message no2");
   });
 });
