1f90ed107f37a098bdc43135bf86601bba159f8a
04alexklink <alex_klink123@hotmail.co.uk>
2021-01-08 17:17:27 +0000

    Connected Express API to the MessageApp Model, and test passes checking expected content is sent as a response

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
 var app = express();
+import MessageApp from './lib/model.js';
 
-app.get('/', function (req, res) {
-    res.send({ val: 'Hello World' })
+var messageApp = new MessageApp('/json/testMessages.json');
+
+app.get('/', async (req, res) => {
+    var messages = messageApp.getAll();
+    res.json(messages);
  })
 
-app.listen(3001)
+app.listen(3001, function() {
+  console.log("Connected");
+})
 
 export default app
