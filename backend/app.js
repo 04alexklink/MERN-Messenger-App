ca9d46cb29afc45ee342956b054b64c8e13b51b9
04alexklink <alex_klink123@hotmail.co.uk>
2021-01-08 14:36:47 +0000

    Used Express to create an API. Installed nyc for code coverage. Installed supertest to allow testing of API. API is not connected yet to the messageapp model, but a test passes for the API to respond with Hello World when a request is sent to '/'

--- backend/app.js
+++ backend/app.js
@@ -1,75 +1,10 @@
-import fs from 'fs';
+import express from 'express';
+var app = express();
 
-class MessageApp {
-  constructor(jsonMessageFile) {
-    this.filepath = jsonMessageFile
-    this.messages = this.filepath ? this.readFromJson() : [];
-  }
+app.get('/', function (req, res) {
+    res.send({ val: 'Hello World' })
+ })
 
-  create(messageContent) {
-    if(messageContent !== undefined && messageContent !== '') {
-      var message = {
-        content : messageContent,
-        id : this._newID(),
-        date : new Date()
-      }
-      this.messages.push(message);
-      this.writeToJson();
-    }
-  }
+app.listen(3001)
 
-  get(messageID) {
-    return this.messages.filter(message => message.id === messageID )[0];
-  }
-
-  getAll() {
-    return this.messages;
-  }
-
-  update(messageID, newContent) {
-    var index = this.messages.findIndex(message => message.id === messageID);
-    if(index >= 0 && newContent !== undefined && newContent !== "") {
-    this.messages[index].content = newContent;
-    this.messages[index].date = new Date();
-    this.writeToJson();
-    } else {
-      return "This message does not exist or updated content is missing."
-    }
-  }
-
-  delete(messageID) {
-    var index = this.messages.findIndex(message => message.id === messageID);
-    if(index >= 0) {
-    this.messages.splice(index,1);
-    this.writeToJson();
-    } else {
-      return "This message did not exist."
-    }
-  }
-  _newID() {
-    if(this.messages.length >= 1) {
-    return this.messages[this.messages.length - 1].id + 1;
-    } else { 
-      return 1;
-    }
-  }
-
-  readFromJson() {
-    return JSON.parse(fs.readFileSync(
-      __dirname+this.filepath,"utf8",(err,data)=>{
-        if (err) throw err;
-      })
-    )
-  }
-  
-  writeToJson() {
-    if (this.filepath) {
-      var jsonItem = JSON.stringify(this.messages)
-      fs.writeFileSync(__dirname+this.filepath, jsonItem, (err) => {
-        if (err) throw err;
-      });
-    }
-  }
-}
-
-export default MessageApp
+export default app
