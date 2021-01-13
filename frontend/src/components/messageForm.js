0a2683d5491e884538b22511d2f7cf562b50e591
04alexklink <alex_klink123@hotmail.co.uk>
2021-01-13 12:05:34 +0000

    Post Messages from form functionality completed. API call mocked using Axios

--- frontend/src/components/messageForm.js
+++ frontend/src/components/messageForm.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
 class MessageForm extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+          currentMessage: ''
+        }
+      }
+
+    changeMessageValue(change){
+        this.setState({
+          currentMessage: change
+        })
+    }
+    componentDidUpdate(){
+        console.log(this.state.currentMessage)
+      }
+
+    processSubmit(e){
+      e.preventDefault();
+      this.props.submitMessage(this.state.currentMessage)
+      this.changeMessageValue('')
+    }
+
   render(){
     return (
-      <form id='message_form'>
-      <textarea id='message_box'></textarea>
+      <form ref='formRef' onSubmit={(e)=>this.processSubmit(e)}>
+      <textarea onChange={(e) => this.changeMessageValue(e.target.value)}
+           value={this.state.currentMessage} id='message_box'></textarea>
       <br/>
       <button type="button" name="Submit" id="submit">Submit</button>
       </form>
--- frontend/src/tests/App.test.js
+++ frontend/src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import MessageApp from './App'
+import MessageApp from '../App';
+import mockAxios from '../__mocks__/axios.js';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { mount } from 'enzyme'
@@ -8,6 +9,13 @@ import { mount } from 'enzyme'
 Enzyme.configure({ adapter: new Adapter()})
 
 describe('App',() => {
+  beforeEach(function(){
+    mockAxios.post.mockImplementation(() =>
+    Promise.resolve({ data: [] }))
+  })
+  afterEach(function(){
+    mockAxios.post.mockClear()
+  }) 
   it('renders without crashing', () => {
     const component = mount(<MessageApp/>);
     expect(component).toMatchSnapshot();
@@ -23,5 +31,12 @@ describe('App',() => {
   it('has a message list', () => {
     const component = mount(<MessageApp/>);
     expect(component.exists('ul#message_list')).toBe(true);
+  });
+  it('posts data and clears message box on submit success', () => {
+    const component = mount(<MessageApp/>);
+    component.find('textarea#message_box').simulate('change', { target: { value: 'Hello' } })
+    component.find('form').simulate('submit')
+    expect(mockAxios.post).toHaveBeenCalledWith("http://localhost:3001/message", {"content": "Hello"});
+    expect(component.instance().refs.messageFormRef.state.currentMessage).toEqual('');
   });
 })
